e6ef44cbaccfcd3a943c23a3320030377445f31a
AmanVerma-03 <101995292+AmanVerma-03@users.noreply.github.com>
2024-07-15 21:39:32 +0530

    Time: 0 ms (100%), Space: 44.3 MB (26.35%) - LeetHub

--- 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.java
+++ 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.java
@@ -7,12 +7,17 @@ class Solution {
             // int mid = (start + end)/2;
             // integer have a fix size , there might be possible that (start + end) thing we are doing exceeds the range of integers in java
              mid = start + (end - start) / 2;
-            if ( letters[mid] <= target)
-                start = mid + 1;
-            else  
-                end = mid - 1;
-            
+             if( target < letters[mid])
+             {
+                end = mid - 1 ;
+             }
+             else
+             {
+                start = mid + 1 ;
+             }
+
         }
-       return start == letters.length ? letters[0] : letters[start];
+        return letters[start % letters.length] ;
+            
     }
 }
