fd2004a3873646372b7faf18f7eca71d5af35fa2
AmanVerma-03 <101995292+AmanVerma-03@users.noreply.github.com>
2024-08-10 04:26:25 +0530

    Time: 27 ms (85.31%), Space: 51.3 MB (65.86%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,21 +1,44 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+  List<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
-        Set<List<Integer>> st = new HashSet<>();
-        
-        for (int i = 0; i < n; i++) {  // Need at least 3 elements to form a triplet
-            Set<Integer> hashSet = new HashSet<>();
-            for (int j = i + 1; j < n; j++) {
-                int k = - (nums[i] + nums[j]);
-                if (hashSet.contains(k)) {
-                    List<Integer> temp = Arrays.asList(nums[i], nums[j], k);
-                    temp.sort(null);  // Sort to avoid duplicates
-                    st.add(temp);
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n - 2; i++) {
+            // Skip duplicate elements for the 'i' index
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+
+                    // Skip duplicate elements for the 'j' index
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+
+                    // Skip duplicate elements for the 'k' index
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
+
+                    j++;
+                    k--;
                 }
-                hashSet.add(nums[j]);
             }
         }
-        
-        return new ArrayList<>(st);
+
+        return ans;
     }
 }
