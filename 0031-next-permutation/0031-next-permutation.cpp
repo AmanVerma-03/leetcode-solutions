e747958cfe9db3b987d04217388e70cb4b9a9304
AmanVerma-03 <101995292+AmanVerma-03@users.noreply.github.com>
2024-08-29 15:25:50 +0530

    Time: 8 ms (14.85%), Space: 14.9 MB (6.26%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int ind = -1 ;
-        int  n = nums.size() ;
-        for(int i = n-2 ; i >=0 ; i--)
-        {
-            if(nums[i] < nums[i+1])
-           {
-               ind = i ;
-               break ;
-           } 
-        }
+        int ind = -1;
+        int n = nums.size();
 
-         if (ind == -1) {
-            reverse(nums.begin(), nums.end());
-        } else {
-            // Find the smallest element greater than nums[ind] from the right
-            for (int i = n - 1; i > ind; i--) {
-                if (nums[i] > nums[ind]) {
-                    swap(nums[i], nums[ind]);
-                    break;
-                }
+        // Step 1: Find the first index from the end where the array is not in descending order.
+        for(int i = n - 2; i >= 0; i--) {
+            if(nums[i] < nums[i + 1]) {
+                ind = i;
+                break;
             }
-
-            // Reverse the subarray from ind + 1 to the end
-            reverse(nums.begin() + ind + 1, nums.end());
         }
-       
+
+        // Step 2: If no such index is found, the array is in descending order,
+        // which means this is the last permutation. So reverse it to get the first permutation.
+        if (ind == -1) {
+            reverse(nums.begin(), nums.end());
+            return; // Add this return statement
+        }
+
+        // Step 3: Find the smallest number greater than nums[ind] to swap with it.
+        for(int i = n - 1; i > ind; i--) {
+            if(nums[i] > nums[ind]) {
+                swap(nums[i], nums[ind]);
+                break;
+            }
+        }
+
+        // Step 4: Reverse the part of the array after the index 'ind' to get the next permutation.
+        reverse(nums.begin() + ind + 1, nums.end());
+
     }
 };
