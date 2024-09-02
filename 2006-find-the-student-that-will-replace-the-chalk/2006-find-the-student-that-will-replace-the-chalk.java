bed6eb1c31bc25abdb3ad2858a4853e7f60782bc
AmanVerma-03 <101995292+AmanVerma-03@users.noreply.github.com>
2024-09-02 10:52:56 +0530

    Time: 2 ms (72.59%), Space: 60.2 MB (20.62%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.java
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.java
@@ -1,14 +1,24 @@
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
-        long accSum = 0;
-        for (int c : chalk) accSum += c;
-
-        k %= accSum;
-
-        for (int i = 0; i < chalk.length; i++) {
-            if (chalk[i] > k) return i;
-            k -= chalk[i];
+        int ans = 0 ;
+        long totalchalk = 0 ;
+        int n = chalk.length ;
+        for(int i = 0 ; i < n ; i++)
+        {
+            totalchalk+=chalk[i] ;
+             // Calculate the total sum of chalk required for one full round
         }
-        return -1; 
+     
+         long rem = (int) (k%totalchalk) ; // since the sequence is cyclic , return k modulo totalchalk
+    
+    for(int i = 0 ; i < n ; i++)
+    {
+        if ( rem < chalk[i]) // find the student where we can replace the chalk ;
+        {
+            return i ;
+        }
+        rem-=chalk[i] ;
+    }
+    return -1 ; 
     }
 }
