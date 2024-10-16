214e5631bda507b9ce90459c3227af2a6cd3d6bb
AmanVerma-03 <101995292+AmanVerma-03@users.noreply.github.com>
2024-10-16 11:22:45 +0530

    Time: 24 ms (36.58%), Space: 20.3 MB (15.49%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,46 +1,46 @@
 class Solution {
 
-private :
-
-void dfs(int node , vector<int>adjls[] , vector<int>&vis)
-{
-    vis[node] = 1 ;
-    for(auto it : adjls[node])
-    if(!vis[it])
-    {
-        dfs(it , adjls , vis) ; 
-    }
-}
-
-
-
-
-
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size() ;
-         vector<int>adjls[n];
-         for(int i = 0 ; i < n ; i++)
-         {
-            for(int j = 0 ; j < n ; j++)
-            {
-                if(isConnected[i][j] == 1 && i != j)
-                {
+        int n = isConnected.size();
+        
+        // Create an adjacency list
+        vector<int> adjls[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1 && i != j) {
                     adjls[i].push_back(j);
-                    adjls[j].push_back(i);
+                    adjls[j].push_back(i);  // Add both directions since it's an undirected graph
                 }
             }
-         }
-         vector<int>vis(n,0);
-         int  cnt = 0 ;
-         for(int i = 0 ; i < n ; i++)
-         {
-            if(!vis[i])
-            {
-                cnt++ ;
-                dfs(i ,adjls , vis);
+        }
+
+        vector<int> vis(n, 0);  // To track visited cities
+        int cnt = 0;  // Count of provinces
+
+        // Iterate over each city
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {  // If the city is unvisited
+                cnt++;  // Found a new province
+                queue<int> q;
+                q.push(i);
+                vis[i] = 1;  // Mark the city as visited
+
+                // Perform BFS
+                while (!q.empty()) {
+                    int node = q.front();
+                    q.pop();
+                    // Visit all neighbors of the current city (node)
+                    for (auto it : adjls[node]) {
+                        if (!vis[it]) {
+                            vis[it] = 1;  // Mark as visited
+                            q.push(it);  // Add the neighbor to the queue for further exploration
+                        }
+                    }
+                }
             }
-         }
-         return cnt ;
+        }
+
+        return cnt;  // Return the number of provinces (connected components)
     }
 };
