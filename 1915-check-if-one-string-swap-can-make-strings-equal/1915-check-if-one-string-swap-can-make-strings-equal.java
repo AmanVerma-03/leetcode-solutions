239fc7f8f7f13fe1be05ae7f3ba224c300491335
AmanVerma-03 <101995292+AmanVerma-03@users.noreply.github.com>
2025-02-05 14:10:27 +0530

    Time: 0 ms (100%), Space: 41.7 MB (58.39%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
@@ -1,37 +1,19 @@
-import java.util.*;
 
 class Solution {
 
-    public boolean check(String s1, String s2) {
-        Set<Character> st = new HashSet<>();
-        for (char i : s1.toCharArray()) {
-            st.add(i);
-        }
-        return s2.chars().anyMatch(ch -> st.contains((char) ch));
-    }
-
     public boolean areAlmostEqual(String s1, String s2) {
-        if (s1.equals(s2)) {
-            return true;
-        }
+        if (s1.equals(s2)) return true;
 
-        int diffCount = 0;
         List<Integer> diffList = new ArrayList<>();
-
         for (int i = 0; i < s1.length(); i++) {
-            if (s1.charAt(i) != s2.charAt(i)) {  
-                diffCount++;
+            if (s1.charAt(i) != s2.charAt(i)) {
                 diffList.add(i);
+                if (diffList.size() > 2) return false;
             }
         }
 
-        if (diffCount != 2) {
-            return false;
-        }
-
-        int i1 = diffList.get(0);
-        int i2 = diffList.get(1);
-
-        return s1.charAt(i1) == s2.charAt(i2) && s1.charAt(i2) == s2.charAt(i1); 
+        return diffList.size() == 2 &&
+               s1.charAt(diffList.get(0)) == s2.charAt(diffList.get(1)) &&
+               s1.charAt(diffList.get(1)) == s2.charAt(diffList.get(0));
     }
 }
