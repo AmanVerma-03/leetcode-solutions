c852c82dff231fc16f0320e46f9fb62dc3b46f50
AmanVerma-03 <101995292+AmanVerma-03@users.noreply.github.com>
2025-02-10 19:31:38 +0530

    Time: 0 ms (100%), Space: 8.3 MB (87.14%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        int n = s.length() ;
+        string ans ;
+       
+        for(int i = 0 ; i  < n ; i++)
+        {
+             if(isdigit(s[i]))
+            {
+               if (!ans.empty()) { 
+                    ans.pop_back(); 
+                }
+            }
+            else
+            ans.push_back(s[i]);
+        }
+        return ans ;
+    }
+};
